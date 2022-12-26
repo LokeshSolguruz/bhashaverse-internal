47925c79a81f30c3c9278a63a1de7aec2404c4e6
mihirjsolguruz <mihirj@solguruz.com>
2022-12-26 22:29:53 +0530

    feat: adding splash screen UI

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../routes/app_routes.dart';
+import '../../utils/constants/app_constants.dart';
+import '../../utils/screen_util/screen_util.dart';
+import '../../utils/theme/app_colors.dart';
 import 'controller/splash_controller.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -17,15 +21,23 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     _splashController = Get.find();
     super.initState();
-    _splashController.calcAvailableSourceAndTargetLanguages();
+    ScreenUtil().init();
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      Get.offNamed(AppRoutes.homeRoute);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: flushOrangeColor,
       body: SafeArea(
         child: Center(
-          child: Text('Welcome'),
+          child: Image.asset(
+            imgSplashLogo,
+            height: 100.toHeight,
+            width: 100.toWidth,
+          ),
         ),
       ),
     );
