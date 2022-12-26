d88edb925d707c18cc79955b1214442d4e077f09
mihirjsolguruz <mihirj@solguruz.com>
2022-12-26 22:26:49 +0530

    feat: adding text styles

---
+++ lib/utils/theme/app_text_style.dart
@@ -0,0 +1,23 @@
+import 'package:bhashaverse/utils/screen_util/screen_util.dart';
+import 'package:bhashaverse/utils/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  final semibold24BalticSea = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 24.toFont,
+    color: balticSea,
+  );
+
+  final light16BalticSea = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16.toFont,
+    color: balticSea,
+  );
+
+  final regular18DolphinGrey = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 18.toFont,
+    color: dolphinGray,
+  );
+}
