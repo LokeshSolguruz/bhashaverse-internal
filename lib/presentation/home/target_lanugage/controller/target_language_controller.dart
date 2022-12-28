6751f98c73a2b1cd51ac7ca20c7a16dc98a98a33
mihirjsolguruz <mihirj@solguruz.com>
2022-12-28 15:17:22 +0530

    feat: adding target language screen

---
+++ lib/presentation/home/target_lanugage/controller/target_language_controller.dart
@@ -0,0 +1,61 @@
+import 'package:get/get.dart';
+
+import '../../../../models/app_language_model.dart';
+import '/utils/constants/app_constants.dart';
+
+class TargetLanguageController extends GetxController {
+  final _selectedLanguage = Rxn<int>();
+
+  int? getSelectedLanguageIndex() {
+    return _selectedLanguage.value;
+  }
+
+  void setSelectedLanguageIndex(int index) {
+    _selectedLanguage.value = index;
+  }
+
+// Get app language list
+  List<AppLanguageModel> _getAppLanguageList() {
+    List<AppLanguageModel> languageList = <AppLanguageModel>[];
+
+    languageList.add(AppLanguageModel(
+      image: imgEnglish,
+      title: AppStrings.english,
+      subTitle: AppStrings.english,
+    ));
+    languageList.add(AppLanguageModel(
+      image: imgHindi,
+      title: AppStrings.hindiNative,
+      subTitle: AppStrings.hindi,
+    ));
+    languageList.add(AppLanguageModel(
+      image: imgMarathi,
+      title: AppStrings.marathiNative,
+      subTitle: AppStrings.marathi,
+    ));
+    languageList.add(AppLanguageModel(
+      image: imgPunjabi,
+      title: AppStrings.punjabiNative,
+      subTitle: AppStrings.punjabi,
+    ));
+    languageList.add(AppLanguageModel(
+      image: imgBengali,
+      title: AppStrings.bengaliNative,
+      subTitle: AppStrings.bengali,
+    ));
+    languageList.add(AppLanguageModel(
+      image: imgTamil,
+      title: AppStrings.tamilNative,
+      subTitle: AppStrings.tamil,
+    ));
+    languageList.add(AppLanguageModel(
+      image: imgKannada,
+      title: AppStrings.kannadaNative,
+      subTitle: AppStrings.kannada,
+    ));
+
+    return languageList;
+  }
+
+  List<AppLanguageModel> getAppLanguageList() => _getAppLanguageList();
+}
