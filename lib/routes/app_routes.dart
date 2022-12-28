f7ebf9e6c6564be18a72c6fc7d1199208e41a2d1
mihirjsolguruz <mihirj@solguruz.com>
2022-12-28 15:19:41 +0530

    feat: adding source and target language routs

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,9 @@
-import 'package:bhashaverse/presentation/home_screen/binding/home_binding.dart';
-import 'package:bhashaverse/presentation/home_screen/home_screen.dart';
+import 'package:bhashaverse/presentation/home/home_screen/binding/home_binding.dart';
+import 'package:bhashaverse/presentation/home/home_screen/home_screen.dart';
+import 'package:bhashaverse/presentation/home/source_language/binding/source_language_binding.dart';
+import 'package:bhashaverse/presentation/home/source_language/source_language_screen.dart';
+import 'package:bhashaverse/presentation/home/target_lanugage/binding/target_language_binding.dart';
+import 'package:bhashaverse/presentation/home/target_lanugage/target_language_screen.dart';
 import 'package:bhashaverse/presentation/onboarding/binding/onboarding_binding.dart';
 import 'package:bhashaverse/presentation/onboarding/onboarding_screen.dart';
 import 'package:bhashaverse/presentation/select_app_language/binding/app_language_binding.dart';
@@ -16,6 +20,9 @@ class AppRoutes {
   static String appLanguageRoute = '/app_language_route';
   static String voiceAssistantRoute = '/voice_assistant_route';
   static String onboardingRoute = '/onboarding_route';
+  static String bottomNavTranslation = '/bottom_nav_translation';
+  static String translateFrom = '/translate_from_route';
+  static String translateTo = '/translate_to_route';
 
   static List<GetPage> pages = [
     GetPage(
@@ -42,6 +49,16 @@ class AppRoutes {
       name: homeRoute,
       page: () => const HomeScreen(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: translateFrom,
+      page: () => const SourceLanguageScreen(),
+      binding: SourceLanguageBinding(),
+    ),
+    GetPage(
+      name: translateTo,
+      page: () => const TargetLanguageScreen(),
+      binding: TargetLanguageBinding(),
     ),
   ];
 }
