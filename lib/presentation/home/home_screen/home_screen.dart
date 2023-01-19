551ccf0f0f89c84a45ee5882c3a183a46b1a23c2
mihirjsolguruz <mihirj@solguruz.com>
2023-01-19 23:55:38 +0530

    fix: setting sticky bottom again in home screen

--- lib/presentation/home/home_screen/home_screen.dart
+++ lib/presentation/home/home_screen/home_screen.dart
@@ -36,6 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: honeydew,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Obx(
           () => Stack(
