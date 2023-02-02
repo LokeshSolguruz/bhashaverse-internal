d1844f774537af38cdd571b1cdb1ede9ed0a82e8
mihirjsolguruz <mihirj@solguruz.com>
2023-02-03 00:03:35 +0530

    fix: increasing transliteration height - scroll indicator height isn't proper in transliteration

--- lib/presentation/home/bottom_nav_screens/bottom_nav_translation/bottom_nav_translation_screen.dart
+++ lib/presentation/home/bottom_nav_screens/bottom_nav_translation/bottom_nav_translation_screen.dart
@@ -248,7 +248,7 @@ class _BottomNavTranslationState extends State<BottomNavTranslation>
           ),
 
           SizedBox(
-            height: 70.toHeight,
+            height: 75.toHeight,
             child: Obx(
               () => Visibility(
                 visible:
