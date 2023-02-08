c14fbefcaedaea4b1625ebf13253b91987445217
mihirjsolguruz <mihirj@solguruz.com>
2023-02-08 11:52:19 +0530

    feat: binding mic stream service

--- lib/presentation/splash_screen/binding/splash_binding.dart
+++ lib/presentation/splash_screen/binding/splash_binding.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 import '../../../common/controller/language_model_controller.dart';
 import '../../../services/translation_app_api_client.dart';
+import '../../../utils/mic_streamer.dart';
 import '../controller/splash_controller.dart';
 
 class SplashBinding extends Bindings {
@@ -9,6 +10,7 @@ class SplashBinding extends Bindings {
   void dependencies() {
     Get.put(TranslationAppAPIClient.getAPIClientInstance(), permanent: true);
     Get.put(LanguageModelController());
+    Get.put(MicStreamer());
     Get.put(SplashController());
   }
 }
