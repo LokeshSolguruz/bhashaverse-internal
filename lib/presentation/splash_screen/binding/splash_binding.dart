7707fb534644ce178c97e70f805ec6667697d6a4
mihirjsolguruz <mihirj@solguruz.com>
2023-02-08 23:16:23 +0530

    feat: binding socket io client class

--- lib/presentation/splash_screen/binding/splash_binding.dart
+++ lib/presentation/splash_screen/binding/splash_binding.dart
@@ -1,8 +1,8 @@
+import 'package:bhashaverse/services/socket_io_client.dart';
 import 'package:get/get.dart';
 
 import '../../../common/controller/language_model_controller.dart';
 import '../../../services/translation_app_api_client.dart';
-import '../../../utils/mic_streamer.dart';
 import '../controller/splash_controller.dart';
 
 class SplashBinding extends Bindings {
@@ -10,7 +10,7 @@ class SplashBinding extends Bindings {
   void dependencies() {
     Get.put(TranslationAppAPIClient.getAPIClientInstance(), permanent: true);
     Get.put(LanguageModelController());
-    Get.put(MicStreamer());
+    Get.put(SocketIOClient());
     Get.put(SplashController());
   }
 }
